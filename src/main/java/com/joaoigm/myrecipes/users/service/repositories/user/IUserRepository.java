bb86d6802fd8a49c7ea874762555b508b6edb08c
João Igor <joao.melo@esx.com.br>
2020-06-22 11:26:23 -0300

    adicionando property para mongodb

--- src/main/java/com/joaoigm/myrecipes/users/service/repositories/user/IUserRepository.java
+++ src/main/java/com/joaoigm/myrecipes/users/service/repositories/user/IUserRepository.java
@@ -2,8 +2,10 @@ package com.joaoigm.myrecipes.users.service.repositories.user;
 
 import com.joaoigm.myrecipes.users.service.models.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
+@Repository
 public interface IUserRepository extends MongoRepository<User, UUID> {
 }
